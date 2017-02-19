6e62564cbf984845ce2e68461b8efc7c1962ea55
Isaac <isaacxpreston@gmail.com>
2017-02-19 07:28:01 -0800

    building searchbar

--- main.js
+++ main.js
@@ -24,11 +24,43 @@ var utils = {
   }
 }
 
+//searchbar
+  //form entry
+  //make axios call to people
+  //if people is not in any of the objects, make axios call to next
+  //if runs out of next, return not found
+    //if next === null || if !next
+  //else make popup with name and info
+
+var searchBar = () => {
+  var formInput = () => {
+    var handleSubmit = () => {
+      console.log('searching for ', input.value)
+      //axios search logic here
+    }
+
+    var handleKeyDown = (input, e) => {
+      if (e.key === 'Enter') {
+        handleSubmit(input.value)
+        input.value = ""
+      }
+    }
+
+    var input = document.createElement('input')
+    input.type = "text"
+    input.placeholder = "search"
+    input.addEventListener('keydown', handleKeyDown.bind(this, input), false)
+    return input
+  }
+  var searchDiv = document.createElement('div')
+  searchDiv.appendChild(formInput())
+  document.body.appendChild(searchDiv)
+}
+
 var popUp = (data) => {
-  console.log(data)
   var popUp = document.createElement('div')
   popUp.id = "popUp"
-  popUp.innerHTML = data
+  popUp.innerHTML = "a popup about " + data
   popUp.addEventListener("click", () => {
     document.body.removeChild(popUp)
   }, false)
@@ -38,13 +70,14 @@ var popUp = (data) => {
   } 
 }
 
-var PersonDetails = (data) => {
-  var el = document.createElement("span")
-  el.innerHTML = data
-  return el
-}
-
 var PersonEntry = (name, url) => {
+  //todo -> make obj instead of var
+  var PersonDetails = (data) => {
+    var details = document.createElement("span")
+    details.innerHTML = data
+    return details
+  }
+
   var el = document.createElement("div")
   el.addEventListener("click", () => {
     axios.get(url)
@@ -55,12 +88,14 @@ var PersonEntry = (name, url) => {
       console.log(err)
     })
   }, false)
+
   utils.styleElement(el, "cool")
   el.appendChild(PersonDetails(name))
   document.body.appendChild(el)
 }
 
 window.onload = () => {
+  searchBar()
   axios.get('http://swapi.co/api/people/')
   .then((res) => {
     res.data.results.forEach((entry) => {
